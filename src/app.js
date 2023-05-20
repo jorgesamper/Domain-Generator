c317f0ec580d273b1f3f3f062658d7d722b40b7f
Jorge Samper <60151922+jorgesamper@users.noreply.github.com>
2023-05-20 21:10:14 +0200

    visual changes.js

--- src/app.js
+++ src/app.js
@@ -19,16 +19,15 @@ window.onload = function() {
 };
 
 function generarDominio() {
-  const pronombres = ["The", "My", "His"];
-  const palabras = ["3D", "print", "printer", "creative3D"];
+  const pronombres = ["The", "My", "Original"];
+  const palabras = ["3D", "print", "creative", "art3D"];
   const extensiones = [".com", ".net", ".org", ".io"];
   let domain = [];
   for (let i = 0; i < pronombres.length; i++) {
     for (let j = 0; j < palabras.length; j++) {
       for (let k = 0; k < extensiones.length; k++) {
-        let randomDomain =
-          "www." + pronombres[i] + palabras[j] + extensiones[k] + "<br>";
-        domain.push(randomDomain);
+            let randomDomain = `<span class="domain">${pronombres[i]}${palabras[j]}${extensiones[k]}</span><br>`;
+        domain += randomDomain;
       }
     }
   }
